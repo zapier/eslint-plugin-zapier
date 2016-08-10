6f1f1cc40713bfa4b959dbdf72b4fc92b73f5444
Jacob Blakely <jacob@codekirei.com>
2016-08-09 17:26:25 -0700

    add error rules

--- index.js
+++ index.js
@@ -25,15 +25,46 @@ module.exports = {
     // possible errors -------------------------------------------------------------
 
     'comma-dangle': 0,
-    'no-console': [2, { 'allow': ['warn', 'error'] }],
 
     // stylistic issues ------------------------------------------------------------
 
     'no-mixed-spaces-and-tabs': [2, false],
 
     // -----------------------------------------------------------------------------
-    // ADDITIONAL RULES
+    // ESLINT RULES
     // -----------------------------------------------------------------------------
+
+    // possible errors -------------------------------------------------------------
+
+    'no-cond-assign': 2,
+    'no-console': 1,
+    'no-constant-condition': 2,
+    'no-control-regex': 2,
+    'no-debugger': 2,
+    'no-dupe-args': 2,
+    'no-dupe-keys': 2,
+    'no-duplicate-case': 2,
+    'no-empty-character-class': 2,
+    'no-empty': 2,
+    'no-ex-assign': 2,
+    'no-extra-boolean-cast': 2,
+    'no-extra-parens': 2,
+    'no-extra-semi': 2,
+    'no-func-assign': 2,
+    'no-inner-declarations': 2,
+    'no-invalid-regexp': 2,
+    'no-irregular-whitespace': 2,
+    'no-negated-in-lhs': 2,
+    'no-obj-calls': 2,
+    'no-prototype-builtins': 0,
+    'no-regex-spaces': 2,
+    'no-sparse-arrays': 2,
+    'no-unexpected-multiline': 2,
+    'no-unreachable': 2,
+    'no-unsafe-finally': 2,
+    'use-isnan': 2,
+    'valid-jsdoc': 0,
+    'valid-typeof': 2,
 
     // best practices --------------------------------------------------------------
 
