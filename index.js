132e2c774cf538f2f713083391c84ab68cfffc2b
Jacob Blakely <jacob@codekirei.com>
2016-08-09 17:37:49 -0700

    add variables rules

--- index.js
+++ index.js
@@ -135,10 +135,17 @@ module.exports = {
 
     // variables -------------------------------------------------------------------
 
+    'init-declarations': 0,
+    'no-catch-shadow': 0,
+    'no-delete-var': 2,
     'no-label-var': 2,
+    'no-restricted-globals': 0,
     'no-shadow': 2,
     'no-shadow-restricted-names': 2,
+    'no-undef': 2,
     'no-undef-init': 2,
+    'no-undefined': 0,
+    'no-unused-vars': 2,
     'no-use-before-define': 2,
 
     // nodejs ----------------------------------------------------------------------
