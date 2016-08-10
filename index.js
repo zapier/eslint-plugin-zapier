8ca601d84ca7cc99ae43b8f3b48891f682b0b16b
Jacob Blakely <jacob@codekirei.com>
2016-08-09 17:38:57 -0700

    add nodejs rules

--- index.js
+++ index.js
@@ -150,7 +150,16 @@ module.exports = {
 
     // nodejs ----------------------------------------------------------------------
 
+    'callback-return': 0,
+    'global-require': 0,
+    'handle-callback-err': 0,
+    'no-mixed-requires': 0,
+    'no-new-require': 0,
+    'no-path-concat': 0,
+    'no-process-env': 0,
     'no-process-exit': 2,
+    'no-restricted-modules': 0,
+    'no-sync': 0,
 
     // stylistic issues ------------------------------------------------------------
 
