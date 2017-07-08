d9d770fc916b2cb62baaae371334402374e596d1
Vitor Balocco <vitorbal@gmail.com>
2017-07-08 20:45:27 +0200

    Add additional jsx-a11y rules
    Add additional jsx-a11y rules that were added to the main repository to sync up

--- rules/jsx-a11y.js
+++ rules/jsx-a11y.js
@@ -4,6 +4,21 @@ module.exports = {
   ],
 
   'rules': {
-    'jsx-a11y/alt-text': 2,
+    'jsx-a11y/alt-text': 'error',
+    'jsx-a11y/aria-role': 'error',
+    'jsx-a11y/interactive-supports-focus': 'error',
+    'jsx-a11y/no-noninteractive-element-interactions': ['error',
+      {
+        'handlers': [
+          'onClick',
+          'onMouseDown',
+          'onMouseUp',
+          'onKeyPress',
+          'onKeyDown',
+          'onKeyUp',
+        ],
+      },
+    ],
+    'jsx-a11y/no-static-element-interactions': 'error',
   },
 };
