2c19a9c8cd53eb9412189459be9b647270032d52
Alejandro Nanez <alejonanez@gmail.com>
2018-07-26 18:18:38 -0500

    Preventing the usage of Object

--- config/rules/flowtype.js
+++ config/rules/flowtype.js
@@ -6,6 +6,9 @@ module.exports = {
   rules: {
     "flowtype/no-types-missing-file-annotation": "error",
     "flowtype/define-flow-type": "error",
-    "flowtype/use-flow-type": "error"
+    "flowtype/use-flow-type": "error",
+    "flowtype/no-weak-types": ["error", {
+      "Object": false,
+    }]
   }
 };
