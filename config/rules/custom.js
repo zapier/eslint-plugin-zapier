28e842888a5c7911a38e707878a87720eb3452e3
ngryman <ngryman@gmail.com>
2018-09-21 18:22:31 +0200

    add rule as a warning

--- config/rules/custom.js
+++ config/rules/custom.js
@@ -1,5 +1,6 @@
 module.exports = {
   rules: {
+    '@zapier/zapier/disallow-default-import-and-module-name-mismatch': 'warn',
     '@zapier/zapier/disallow-lodash-get-dot-notation': 'error',
   },
 };
