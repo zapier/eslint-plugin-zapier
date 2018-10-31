4f7f2458cb1b087ec9d180cdf742f294b656a753
ruebel <randy.uebel@gmail.com>
2018-10-31 07:52:54 -0400

    :sparkles: add jsx-sort-props rule

--- config/rules/react.js
+++ config/rules/react.js
@@ -11,10 +11,11 @@ module.exports = {
 
   'rules': {
     'react/forbid-foreign-prop-types': 'error',
+    'react/jsx-boolean-value': ['error', 'always'],
     'react/jsx-no-undef': 'error',
+    'react/jsx-sort-props': ['error', { 'ignoreCase': true }],
     'react/jsx-uses-react': 'error',
     'react/jsx-uses-vars': 'error',
-    'react/jsx-boolean-value': ['error', 'always'],
     'react/react-in-jsx-scope': 'error',
   },
 };
